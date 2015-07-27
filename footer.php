9c0e5b18c1acbd0ed578d19d6b3453c676ebb3ce
Efrain <saltflatsfilmfestival@gmail.com>
2015-07-27 08:03:41 -0600

    hopefully mobile friendly, and registration form

--- footer.php
+++ footer.php
@@ -5,5 +5,14 @@
 			<a href="http://www.kidsfindjoy.org">Joy Foundation</a>
 			&copy; 2015 - All Rights Reserved.
 		</p>
+		<div class="social-media">
+				<a href="https://facebook.com/saltflatsfilmfestival">
+					<i class="fa fa-facebook-square fa-2x"></i>
+				</a>
+				<a href="https://twitter.com/saltflatsfilms">
+					<i class="fa fa-twitter-square fa-2x"></i>
+				</a>
+			</div>
 	</div>
+	
 </div>
