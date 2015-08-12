367acf4a71ac88f756134b81344c25ff6827305c
Efrain <saltflatsfilmfestival@gmail.com>
2015-08-11 18:36:25 -0600

    added contact fix

--- contact.php
+++ contact.php
@@ -11,14 +11,16 @@
 			<h3 class="text-center title">Get In Touch</h3>
 			<div class="row">
 				<div class="col-md-5">
+					<div class="content-left">
 						<?php include_once('contact_form.php')?>
+					</div>
 				</div>
 				<div class="col-md-7">
 					<p class="special">Contact Information</p>
-						<p><a href="mailto:saltflatsfilmfestival@gmail.com"><i class="fa fa-envelope-o"></i>
-						email: saltflatsfilmfestival@gmail.com</a></p>
-						<p><i class="fa fa-phone"></i>  
-						phone: (801) 797-9386</p>
+					<p><a href="mailto:saltflatsfilmfestival@gmail.com"><i class="fa fa-envelope-o"></i>
+					email: saltflatsfilmfestival@gmail.com</a></p>
+					<p><i class="fa fa-phone"></i>  
+					phone: (801) 797-9386</p>
 					<img src="/img/sunrise.jpg" 
 					class="img-responsive center-block" ></img>
 				</div>
