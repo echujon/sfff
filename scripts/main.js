e8c1a133b54a7d1241d8161f7b7bb949f99cb32d
Efrain <saltflatsfilmfestival@gmail.com>
2015-10-08 01:23:16 -0600

    added more sponsor stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,17 @@
 $(function() {
-	var marginTop = parseInt($('#content').css('margin-top'));
-	$(".menu-button").on("click", function(e) {
-		if (!$('.menu-button').hasClass('display'))
-		{
-			var a =  marginTop +
-			 parseInt($('#nav').height())-5;
-			
-			$('#nav').slideDown();
-			
-		}
-		else
-		{
-			$('#nav').slideUp(function() {
-				//$('#content').css('margin-top', marginTop);
-			}); 
-		}
-		$('.menu-button').toggleClass('display');
-			
+	$(".become-sponsor i").on("click", collapseToggle);
+	$('#sponsor-info').on('shown.bs.collapse', function () {
+		var sponsorTop  = $('#sponsor-info').offset().top;
+		$('body, html').animate({
+        scrollTop: sponsorTop
+		}, 1000);
+    	return false;
 	});
+	
+	function collapseToggle()
+	{
+		$('#sponsor-info').collapse('toggle');
+		$(".become-sponsor i").toggleClass("fa-plus-square-o fa-minus-square-o");
+	
+	}
 });
