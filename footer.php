9ad1705e6642386a46b2dacd0fa17f98e6bc7943
Efrain <fchujon@gmail.com>
2016-02-10 18:50:31 -0700

    added the copyright

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<p>
 			Salt Flats Film Festival - presented by the 
 			<a href="http://www.kidsfindjoy.org">Joy Foundation</a>
-			&copy; 2015 - All Rights Reserved.
+			&copy; 2016 - All Rights Reserved.
 		</p>
 		<div class="social-media">
 				<a href="https://facebook.com/saltflatsfilmfestival">
