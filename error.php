279bf41659059a7c84582da93b09c12a6959cc68
Efrain <fchujon@gmail.com>
2016-06-25 00:54:05 -0600

    added error handling

---
+++ error.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+ <?php include_once('links.php')?> 
+ <?php include_once("tracking.php") ?>
+  <title>Error Page</title>
+ </head>
+ <body>
+  <?php include_once('header.php')?> 
+  <div class="container">
+	 <div id="content">
+	 	<h3 class="text-center title">Error Page</h3>
+	 	<div class="row">
+			<div class="text-center">
+				The page you requested doesn't exist.
+			</div>
+		</div>
+	</div>
+ <?php include_once('footer.php')?> 
+ </body>
+</html>
